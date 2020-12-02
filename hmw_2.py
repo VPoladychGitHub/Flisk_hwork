9803898026dc5f32ad8a32389c17e29c799b6075
VPoladychGitHub <VPoladych@ukr.net>
2020-12-02 23:23:26 +0200

    hmw_2.py

--- hmw_2.py
+++ hmw_2.py
@@ -1,6 +1,14 @@
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
 app = Flask(__name__)
 
-@app.route('/')     
-def hello_world():
-    return 'Hello, World!'
+@app.route('/requirements')
+def requirements():
+    f = open('requirements.txt', 'r')
+    s: str = ''
+    for line in f.readlines():
+        s = s + str(line) + '\n'
+    f.close()
+    return s
+
+# python -m flask run
