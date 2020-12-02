40c92a30e76168c87d329d520e2ec18028d84550
VPoladychGitHub <VPoladych@ukr.net>
2020-12-02 20:11:12 +0200

    add

--- read_file.py
+++ read_file.py
@@ -1,9 +1,18 @@
-@app.route('/upload/', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        file = request.files['file']
-    if file:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        a = 'file uploaded'
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+
+# @app.route('/upload/', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST':
+#         file = request.files['file']
+#     if file:
+#         filename = secure_filename(file.filename)
+#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         a = 'file uploaded'
     return render_template('upload.html', data = a)
